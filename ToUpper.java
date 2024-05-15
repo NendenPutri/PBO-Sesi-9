c77cf5db320d4f72ecb92fbf95763db2acebcd41
NendenPutri <153110457+NendenPutri@users.noreply.github.com>
2024-05-16 01:31:02 +0700

    Add files via upload

---
+++ ToUpper.java
@@ -0,0 +1,10 @@
+public class ToUpper {
+    public static void main(String[] args) {
+        String stringOri = "Nenden Putri Nurul Susanti";
+        String stringLower = stringOri.toUpperCase();
+        System.out.println("Sebelum di gunakaan toLowerCase:" + stringOri);
+        System.out.println("Sebelum di gunakaan toLowerCase:" + stringLower);
+
+    }
+
+}
